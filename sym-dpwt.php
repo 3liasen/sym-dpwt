859ef02ea55eeafdb85128ece10b833d412c9f4b
3liasen <jan.eliasen@gmail.com>
2025-09-26 12:26:20 +0200

    initial commit: create Sym DPWT plugin structure and bootstrap function

---
+++ sym-dpwt.php
@@ -0,0 +1,43 @@
+﻿<?php
+/**
+ * Plugin Name:       Sym DPWT
+ * Plugin URI:        https://example.com/plugins/sym-dpwt
+ * Description:       TODO: Describe the purpose of Sym DPWT.
+ * Version:           0.1.0
+ * Requires at least: 6.0
+ * Requires PHP:      7.4
+ * Author:            TODO: Your Name
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       sym-dpwt
+ * Domain Path:       /languages
+ *
+ * @package SymDpwt
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+if ( ! defined( 'SYM_DPWT_VERSION' ) ) {
+    define( 'SYM_DPWT_VERSION', '0.1.0' );
+}
+
+if ( ! defined( 'SYM_DPWT_PLUGIN_FILE' ) ) {
+    define( 'SYM_DPWT_PLUGIN_FILE', __FILE__ );
+}
+
+if ( ! defined( 'SYM_DPWT_PLUGIN_DIR' ) ) {
+    define( 'SYM_DPWT_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+}
+
+if ( ! defined( 'SYM_DPWT_PLUGIN_URL' ) ) {
+    define( 'SYM_DPWT_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+}
+
+/**
+ * Bootstraps the plugin.
+ */
+function sym_dpwt_bootstrap() {
+    // TODO: Register hooks, load text domain, and include additional files here.
+}
+
+add_action( 'plugins_loaded', 'sym_dpwt_bootstrap' );
